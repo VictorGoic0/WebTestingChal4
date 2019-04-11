75bf17a961fbe69fb5721ffdbab05b6d83036325
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-11 18:11:18 -0400

    Test case added for retrieving all posts

--- router/posts-model.spec.js
+++ router/posts-model.spec.js
@@ -5,6 +5,16 @@ describe("posts model", () => {
   beforeEach(async () => {
     await db("posts").truncate();
   });
+  it("should retrieve all posts", async () => {
+    await posts.create({
+      username: "username",
+      description: "Random description"
+    });
+    await posts.create({ username: "username2", description: "description2" });
+    const postlist = await posts.find();
+    expect(postlist).toHaveLength(2);
+  });
+  it("should retrieve post by id", async () => {});
   it("should create the provided post", async () => {
     await posts.create({
       username: "username",
@@ -13,8 +23,6 @@ describe("posts model", () => {
     const postlist = await db("posts");
     expect(postlist).toHaveLength(1);
   });
-  it("should retrieve post by id", async () => {});
-  it("should retrieve all posts", async () => {});
   it("should delete the post by id", async () => {});
   it("should edit the post", async () => {});
 });
