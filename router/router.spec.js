f8cc99a234f3b8d278bbc2ae5b94fe382e34455f
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-11 18:25:38 -0400

    Added tests for POST and DEL

--- router/router.spec.js
+++ router/router.spec.js
@@ -11,5 +11,27 @@ describe("router.js", () => {
       const response = await request(router).get("/");
       expect(response.type).toBe("application/json");
     });
+    it("should return an array", async () => {
+      const response = await request(router).get("/");
+      expect(response.isArray()).toBeTruthy();
+    });
+  });
+  describe("POST /", () => {
+    it("should create and send back a post", async () => {
+      const response = await request(router).post("/");
+      expect(response.body.username).toBeTruthy();
+    });
+  });
+  describe("DEL /id", () => {
+    it("should delete the post", async () => {
+      const posts = await request(router).get("/");
+      const removePost = await request(router).del("/1");
+      const posts2 = await request(router).get("/");
+      expect(posts.length - posts2.length).toBe(1);
+    });
+    it("should send back the deleted post", async () => {
+      const response = await request(router).del("/2");
+      expect(response.body.username).toBeTruthy();
+    });
   });
 });
