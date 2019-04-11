8c2da7848058ed689f3a794797e9c635e28a784e
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-11 17:39:00 -0400

    Migration file created for posts table. Migrations for both dev db and testing db ran

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/hobbits.db3"
+      filename: "./data/posts.db3"
     },
     useNullAsDefault: true,
     migrations: {
